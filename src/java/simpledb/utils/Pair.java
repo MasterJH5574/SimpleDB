990c33fc6989a594cb8af57334402d1ee5ec1fd3
Ruihang Lai <lairuihangdongdong@qq.com>
2021-04-07 21:35:08 +0800

    [Util] Add pair (#18)
    * [Util] Add pair
    
    * [Util] Rename package name

---
+++ src/java/simpledb/utils/Pair.java
@@ -0,0 +1,11 @@
+package simpledb.utils;
+
+public class Pair<Type1, Type2> {
+  public Type1 first;
+  public Type2 second;
+
+  public Pair(Type1 first, Type2 second) {
+    this.first = first;
+    this.second = second;
+  }
+}
