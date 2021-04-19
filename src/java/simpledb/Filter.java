033e84adf0840634e402c6cd8ae2151dabfefca3
Ruihang Lai <lairuihangdongdong@qq.com>
2021-04-19 22:32:09 +0800

    [Lab3] Implement operator Filter (#28)

--- src/java/simpledb/Filter.java
+++ src/java/simpledb/Filter.java
@@ -10,35 +10,45 @@ public class Filter extends Operator {
   private static final long serialVersionUID = 1L;
 
   /**
+   * @see Filter@constructor
+   */
+  private final Predicate p;
+  /**
+   * An iterator used for scanning the table to be filtered
+   */
+  private DbIterator child;
+
+  /**
    * Constructor accepts a predicate to apply and a child operator to read tuples to filter from.
    *
    * @param p     The predicate to filter tuples with
    * @param child The child operator
    */
   public Filter(Predicate p, DbIterator child) {
-    // some code goes here
+    this.p = p;
+    this.child = child;
   }
 
   public Predicate getPredicate() {
-    // some code goes here
-    return null;
+    return p;
   }
 
   public TupleDesc getTupleDesc() {
-    // some code goes here
-    return null;
+    return child.getTupleDesc();
   }
 
   public void open() throws DbException, NoSuchElementException, TransactionAbortedException {
-    // some code goes here
+    super.open();
+    child.open();
   }
 
   public void close() {
-    // some code goes here
+    child.close();
+    super.close();
   }
 
   public void rewind() throws DbException, TransactionAbortedException {
-    // some code goes here
+    child.rewind();
   }
 
   /**
@@ -51,19 +61,24 @@ public class Filter extends Operator {
    */
   protected Tuple fetchNext()
       throws NoSuchElementException, TransactionAbortedException, DbException {
-    // some code goes here
+    while (child.hasNext()) {
+      Tuple tuple = child.next();
+      if (p.filter(tuple)) {
+        return tuple;
+      }
+    }
     return null;
   }
 
   @Override
   public DbIterator[] getChildren() {
-    // some code goes here
-    return null;
+    return new DbIterator[]{child};
   }
 
   @Override
   public void setChildren(DbIterator[] children) {
-    // some code goes here
+    assert children.length >= 1;
+    child = children[0];
   }
 
 }
