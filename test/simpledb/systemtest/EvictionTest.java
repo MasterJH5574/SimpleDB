f9e4929b3f5da4de0eecfc40a70f9a75b3cee9fd
Ruihang Lai <lairuihangdongdong@qq.com>
2021-06-14 08:20:16 +0800

    [Lab5] Initialize

--- test/simpledb/systemtest/EvictionTest.java
+++ test/simpledb/systemtest/EvictionTest.java
@@ -1,14 +1,25 @@
 package simpledb.systemtest;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
 import java.io.IOException;
+import java.util.Arrays;
 import junit.framework.Assert;
 import org.junit.Test;
 import simpledb.Database;
 import simpledb.DbException;
 import simpledb.HeapFile;
+import simpledb.Insert;
+import simpledb.IntField;
 import simpledb.SeqScan;
+import simpledb.Transaction;
 import simpledb.TransactionAbortedException;
 import simpledb.TransactionId;
+import simpledb.Tuple;
+import simpledb.TupleDesc;
+import simpledb.TupleIterator;
+import simpledb.Utility;
 
 /**
  * Creates a heap file with 1024*500 tuples with two integer fields each.  Clears the buffer pool,
@@ -45,42 +56,43 @@ public class EvictionTest extends SimpleDbTestBase {
     }
   }
 
-//    public static void insertRow(HeapFile f, Transaction t) throws DbException,
-//            TransactionAbortedException {
-//        // Create a row to insert
-//        TupleDesc twoIntColumns = Utility.getTupleDesc(2);
-//        Tuple value = new Tuple(twoIntColumns);
-//        value.setField(0, new IntField(-42));
-//        value.setField(1, new IntField(-43));
-//        TupleIterator insertRow = new TupleIterator(Utility.getTupleDesc(2), Arrays.asList(new Tuple[]{value}));
-//
-//        // Insert the row
-//        Insert insert = new Insert(t.getId(), insertRow, f.getId());
-//        insert.open();
-//        Tuple result = insert.next();
-//        assertEquals(SystemTestUtil.SINGLE_INT_DESCRIPTOR, result.getTupleDesc());
-//        assertEquals(1, ((IntField)result.getField(0)).getValue());
-//        assertFalse(insert.hasNext());
-//        insert.close();
-//    }
-//
-//    public static boolean findMagicTuple(HeapFile f, Transaction t)
-//            throws DbException, TransactionAbortedException {
-//        SeqScan ss = new SeqScan(t.getId(), f.getId(), "");
-//        boolean found = false;
-//        ss.open();
-//        while (ss.hasNext()) {
-//            Tuple v = ss.next();
-//            int v0 = ((IntField)v.getField(0)).getValue();
-//            int v1 = ((IntField)v.getField(1)).getValue();
-//            if (v0 == -42 && v1 == -43) {
-//                assertFalse(found);
-//                found = true;
-//            }
-//        }
-//        ss.close();
-//        return found;
-//    }
+  public static void insertRow(HeapFile f, Transaction t)
+      throws DbException, TransactionAbortedException {
+    // Create a row to insert
+    TupleDesc twoIntColumns = Utility.getTupleDesc(2);
+    Tuple value = new Tuple(twoIntColumns);
+    value.setField(0, new IntField(-42));
+    value.setField(1, new IntField(-43));
+    TupleIterator insertRow = new TupleIterator(Utility.getTupleDesc(2),
+        Arrays.asList(new Tuple[]{value}));
+
+    // Insert the row
+    Insert insert = new Insert(t.getId(), insertRow, f.getId());
+    insert.open();
+    Tuple result = insert.next();
+    assertEquals(SystemTestUtil.SINGLE_INT_DESCRIPTOR, result.getTupleDesc());
+    assertEquals(1, ((IntField) result.getField(0)).getValue());
+    assertFalse(insert.hasNext());
+    insert.close();
+  }
+
+  public static boolean findMagicTuple(HeapFile f, Transaction t)
+      throws DbException, TransactionAbortedException {
+    SeqScan ss = new SeqScan(t.getId(), f.getId(), "");
+    boolean found = false;
+    ss.open();
+    while (ss.hasNext()) {
+      Tuple v = ss.next();
+      int v0 = ((IntField) v.getField(0)).getValue();
+      int v1 = ((IntField) v.getField(1)).getValue();
+      if (v0 == -42 && v1 == -43) {
+        assertFalse(found);
+        found = true;
+      }
+    }
+    ss.close();
+    return found;
+  }
 
   /**
    * Make test compatible with older version of ant.
